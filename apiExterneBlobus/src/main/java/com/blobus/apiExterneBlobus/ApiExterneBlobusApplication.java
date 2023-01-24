a7026835bcc573df0fe26f19752220f8f10ee7aa
layefaye <layefayenevrose@gmail.com>
2023-01-24 13:17:13 +0100

    first commit

---
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/ApiExterneBlobusApplication.java
@@ -0,0 +1,13 @@
+package com.blobus.apiExterneBlobus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExterneBlobusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExterneBlobusApplication.class, args);
+	}
+
+}
---
+++ apiExterneBlobus/src/test/java/com/blobus/apiExterneBlobus/ApiExterneBlobusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blobus.apiExterneBlobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExterneBlobusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
