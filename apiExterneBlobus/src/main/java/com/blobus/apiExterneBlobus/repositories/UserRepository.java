079c966341e919ae68291dbb370bf796f7d6dc37
bibiche-97 <abymag.diouf@gmail.com>
2023-01-27 16:22:01 +0100

    crud_user

---
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.blobus.apiExterneBlobus.repositories;
+
+import com.blobus.apiExterneBlobus.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserByPhoneNumber(String phoneNumber);
+}
