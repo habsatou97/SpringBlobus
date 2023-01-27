d113c1edf0d12cad3ff1dbde4436f5370af712e9
habsatou97 <thiam.habsatou@ugb.edu.sn>
2023-01-27 13:32:28 +0100

    Add Model TransfertAccount

---
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/models/TransferAccount.java
@@ -0,0 +1,27 @@
+package com.blobus.apiExterneBlobus.models;
+
+import com.blobus.apiExterneBlobus.models.enumerations.WalletType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "transferAccounts")
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+
+
+public class TransferAccount {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private double balance;
+    private int encryptedPinCode;
+    private WalletType walletType;
+    boolean is_active;
+
+
+}
