c34e596f9c776ff2a8c6fc703ae3c28ff2e402cd
el sedy ba <elsedy000@gmail.com>
2023-01-31 13:16:53 +0000

    definition de ResponseCashInTransactionDto pour envoyer une reponse apres une transaction

---
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/dto/ResponseCashInTransactionDto.java
@@ -0,0 +1,18 @@
+package com.blobus.apiExterneBlobus.dto;
+
+import com.blobus.apiExterneBlobus.models.enums.TransactionStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
+public class ResponseCashInTransactionDto {
+    private String reference;
+    private TransactionStatus status;
+    private Long transactionId;
+    private String errorCode;
+    private String errorMessage;
+}
