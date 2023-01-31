5daaf8f1ed0aedafef75bc1ca41b0be67b71407c
el sedy ba <elsedy000@gmail.com>
2023-01-31 13:22:11 +0000

    ajout methode findByPhoneNumberAndWalletType

--- apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/repositories/TransferAccountRepository.java
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/repositories/TransferAccountRepository.java
@@ -1,7 +1,12 @@
 package com.blobus.apiExterneBlobus.repositories;
 
 import com.blobus.apiExterneBlobus.models.Account;
+import com.blobus.apiExterneBlobus.models.enums.WalletType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransferAccountRepository extends JpaRepository<Account,Long> {
+    // pour recuperer un compte via le numero de telephone et le type de compte
+    Optional<Account> findByPhoneNumberAndWalletType(String phoneNumbber, WalletType walletType);
 }
