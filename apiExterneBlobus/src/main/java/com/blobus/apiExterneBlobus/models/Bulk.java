5d7642721a5aec2a47d01f4f75039beb5fbf882e
el sedy ba <elsedy000@gmail.com>
2023-02-01 09:21:29 +0000

    modification entite Bulk

--- apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/models/Bulk.java
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/models/Bulk.java
@@ -1,16 +1,19 @@
 package com.blobus.apiExterneBlobus.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.catalina.LifecycleState;
 import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 import java.util.List;
+@Entity
+@Table(name = "bulks")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bulk {
     @Id
     @GeneratedValue
