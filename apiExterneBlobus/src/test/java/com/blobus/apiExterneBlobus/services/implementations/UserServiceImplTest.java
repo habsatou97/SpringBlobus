cadbd61e0d1a0f1efc457469c248be777c71ce92
bibiche-97 <abymag.diouf@gmail.com>
2023-02-02 17:01:34 +0100

    Test unitaire UserController & UserService

--- apiExterneBlobus/src/test/java/com/blobus/apiExterneBlobus/services/implementations/UserServiceImplTest.java
+++ apiExterneBlobus/src/test/java/com/blobus/apiExterneBlobus/services/implementations/UserServiceImplTest.java
@@ -2,86 +2,148 @@ package com.blobus.apiExterneBlobus.services.implementations;
 
 import com.blobus.apiExterneBlobus.models.User;
 import com.blobus.apiExterneBlobus.models.enums.Role;
+import com.blobus.apiExterneBlobus.repositories.AccountRepository;
 import com.blobus.apiExterneBlobus.repositories.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.OptionalAssert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 
 @RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class UserServiceImplTest {
 
-    @Autowired
+    @Mock
     private UserRepository userRepository;
+    @Mock
+    private AccountRepository accountRepository;
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private UserRepository repository;
 
-    @Test
-    @AutoConfigureTestDatabase
-    void addSingleUser() {
-        final User user=createUser();
-        try {
-            userRepository.save(user);
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        finally {
-            Assertions.assertThat(userRepository.findById(user.getId()))
-                    .isNotEmpty();
-        }
+    @BeforeEach
+    public void setUp() throws Exception  {
+       userService = new UserServiceImpl(userRepository,accountRepository);
     }
 
-   @Test
-    void updateSingleUser() {
-
-        User user =userRepository.findById(Long.valueOf(102)).orElseThrow();
-        user.setLastName("Barry");
-        user.setFirstName("Dame");
-        user.setEmail("barry.pape-dame@avimtoo.com");
-        user.setRoles(Collections.singletonList(Role.RETAILER));
-        userRepository.save(user);
-       OptionalAssert<User> userOptionalAssert = Assertions.assertThat(userRepository.findById(user.getId()));
-       //Assertions.assertThat(userRepository.count()).isNotZero();
-    }
-/*
     @Test
     void getAllUsers() {
-    }
+        // when
+        userService.getAllUsers();
 
+        // verify
+        verify(userRepository).findAll();
+    }
     @Test
     void getOneUser() {
-    }
 
-    @Test
-    void deleteUser() {
-    }
+        // when
+        userService.getOneUser(Long.valueOf(1));
+        // verify
 
-    @Test
-    void getUserProfileByMsisdn() {
+        verify(userRepository).findById(Long.valueOf(1));
     }
 
     @Test
     void getAllRetailer() {
+        //when
+        userService.getAllRetailer();
+        //verify
+        verify(userRepository).findAll();
     }
+
+    @Test
+    @AutoConfigureTestDatabase
+    void addSingleUser()  throws Exception{
+        // given
+        User user=createUser();
+        //when
+        userService.addSingleUser(user);
+        // then
+      /*  assertThatThrownBy(()->
+                userService.addSingleUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Oups! cette email "+ user.getEmail()+" existe deja");
 */
+        ArgumentCaptor<User> userArgumentCaptor=
+                ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userArgumentCaptor.capture());
+        User capturedUser= userArgumentCaptor.getValue();
+        Assertions.assertThat(capturedUser).isEqualTo(user);
+/*
+        Assertions.assertThat(userRepository.findById(user.getId()))
+                    .isNotEmpty();
+*/
+    }
+
+   @Test
+    void updateSingleUser() {
+            String email ="barry.pape-dame@avimtoo.com";
+        User user =repository.findById(Long.valueOf(102)).orElseThrow();
+        user.setLastName("Barry");
+        user.setFirstName("Dame");
+        user.setEmail(email);
+        user.setRoles(Collections.singletonList(Role.RETAILER));
+        repository.save(user);
+        Assertions.assertThat(repository.findById(user.getId())).isNotEmpty();
+    }
+
+
+   /* @Test
+    @Disabled
+    void deleteUser() {
+        Mockito.when(userRepository.findById(getUser().getId())).thenReturn(Optional.of(getUser()));
+    }
+
+    @Test
+    void getUserProfileByMsisdn() {
+        String phoneNumber = "782654426";
+        userService.getUserProfileByMsisdn(phoneNumber);
+      verify(accountRepository).getAccountByPhoneNumber(phoneNumber);
+        //given
+
+    }*/
+
+    private User getUser(){
+        return repository.findById(Long.valueOf(1)).orElseThrow();
+    }
+
+
     private User createUser(){
-        String email = "rtdgvtf.-gvfuhz@avimtoo.com";
+        String email = "bibiche.diouf@avimtoo.com";
         User user = new User(
-                "Ablaye",
-                "Faye",
+                6l,
+                "El Seydi",
+                "Ba",
                 email ,
                 Role.RETAILER,
-                "789478566u",
-                "754516329",
-                        "fzivbeycjd",
-                "fohfr52578@");
+                "vimto1245",
+                "718954362",
+                "fzivbedfegjd",
+                "fohfgfyf78");
         return user;
     }
 
