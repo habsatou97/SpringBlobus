ac7519529b09532dd324d4fc385588cf3fd1330f
el sedy <elsedy000@gmail.com>
2023-02-14 13:18:28 +0000

    integration du cryptage et decryptage dans les transactions

--- apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/controllers/KeyController.java
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/controllers/KeyController.java
@@ -1,6 +1,7 @@
 package com.blobus.apiExterneBlobus.controllers;
 
-
+//import com.blobus.apiExterneBlobus.models.Keye;
+//import com.blobus.apiExterneBlobus.services.implementations.KeyServiceImpl;
 import com.blobus.apiExterneBlobus.dto.DecryptDto;
 import com.blobus.apiExterneBlobus.dto.KeyDto;
 import com.blobus.apiExterneBlobus.services.interfaces.KeyGeneratorService;
@@ -32,24 +33,15 @@ public class KeyController {
 
 
     @PostMapping("crypt")
-    public ResponseEntity<DecryptDto> encrypt(@RequestBody DecryptDto decryptDto)
-                                     throws NoSuchAlgorithmException,
-                                            NoSuchPaddingException,
-                                            IllegalBlockSizeException,
-                                            NoSuchAlgorithmException,
-                                            IOException,
-                                            BadPaddingException,
-                                            InvalidKeySpecException,
-                                            InvalidKeyException {
+    public ResponseEntity<DecryptDto> encrypt(@RequestBody DecryptDto decryptDto) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
         //key.getKey();
-        DecryptDto decryptDto1=new DecryptDto();
-        decryptDto.setEncryptedPinCode(keyGenerator.encrypt(decryptDto1));
-        return ResponseEntity.ok().body(decryptDto);
+        DecryptDto decryptDto1 = new DecryptDto();
+        decryptDto1.setEncryptedPinCode(keyGenerator.encrypt(decryptDto));
+         return ResponseEntity.ok().body(decryptDto1);
     }
     @PostMapping("decrypt")
-    public String decrypt(@RequestBody  DecryptDto decryptDto) throws NoSuchPaddingException,
-            IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeyException {
-        return keyGenerator.decrypt(decryptDto);
+    public String decrypt(@RequestBody  DecryptDto decryptDto) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, IOException, BadPaddingException, InvalidKeyException {
+         return keyGenerator.decrypt(decryptDto);
     }
 }
 
