b355c3609d7808b613861afb1dab00826a4e5cb7
layefaye <layefayenevrose@gmail.com>
2023-02-15 17:57:49 +0100

    finalizations

--- apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/repositories/CustomerRepository.java
+++ apiExterneBlobus/src/main/java/com/blobus/apiExterneBlobus/repositories/CustomerRepository.java
@@ -9,9 +9,4 @@ import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    //public static final String FIND_CUSTOMERS = "SELECT first_name, last_name, phone_number, email FROM customers";
-    //@Query(value = FIND_CUSTOMERS, nativeQuery = true)
-    //public List<Customer> findAllDto();
-
-
 }
