531330d818d66f6533bb91d9ecf770b280b3a31f
layefaye <layefayenevrose@gmail.com>
2023-02-27 16:42:29 +0000

    finalization

--- src/main/java/com/blobus/apiexterneblobus/ApiExterneBlobusApplication.java
+++ src/main/java/com/blobus/apiexterneblobus/ApiExterneBlobusApplication.java
@@ -39,10 +39,10 @@ public class ApiExterneBlobusApplication
 
 
 	@Bean
-	 public CorsFilter corsFilter() {
+	public CorsFilter corsFilter() {
 	 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	 	corsConfiguration.setAllowCredentials(true);
-	 	corsConfiguration.setAllowedOrigins(List.of("http://blobus-backend-wepapi.azurewebsites.net","http://localhost:44311","http://localhost:4200","http://192.168.1.146:3000","http://192.168.197.233:3000"));
+	 	corsConfiguration.setAllowedOrigins(List.of("https://blobus-backend-wepapi.azurewebsites.net","https://localhost:44311","http://localhost:4200","http://192.168.1.146:3000","http://192.168.197.233:3000"));
 	 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 	 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 	 			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
