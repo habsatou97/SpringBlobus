37a627dc4048c211d5ae21c12edd77c7ac1ccaf4
layefaye <layefayenevrose@gmail.com>
2023-02-28 13:45:47 +0000

    finalization

--- src/main/java/com/blobus/apiexterneblobus/ApiExterneBlobusApplication.java
+++ src/main/java/com/blobus/apiexterneblobus/ApiExterneBlobusApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 import java.security.KeyPair;
@@ -37,22 +39,32 @@ public class ApiExterneBlobusApplication
 		writeToFileString(pub_key, priv_key);
 	}
 
-
 	@Bean
-	public CorsFilter corsFilter() {
-	 	CorsConfiguration corsConfiguration = new CorsConfiguration();
-	 	corsConfiguration.setAllowCredentials(true);
-	 	corsConfiguration.setAllowedOrigins(List.of("http://blobus-backend-wepapi.azurewebsites.net","https://blobus-backend-wepapi.azurewebsites.net","http://localhost:44311","https://localhost:44311","http://localhost:4200","http://192.168.1.146:3000","http://192.168.197.233:3000"));
-	 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-	 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-	 			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-	 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-	 			"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-	 	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-	 	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-	 	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-	 	return new CorsFilter(urlBasedCorsConfigurationSource);
-	 }
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+//
+//	@Bean
+//	public CorsFilter corsFilter() {
+//	 	CorsConfiguration corsConfiguration = new CorsConfiguration();
+//	 	corsConfiguration.setAllowCredentials(true);
+//	 	corsConfiguration.setAllowedOrigins(List.of("http://blobus-backend-wepapi.azurewebsites.net","https://blobus-backend-wepapi.azurewebsites.net","http://localhost:44311","https://localhost:44311","http://localhost:4200","http://192.168.1.146:3000","http://192.168.197.233:3000"));
+//	 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+//	 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+//	 			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+//	 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+//	 			"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+//	 	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//	 	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//	 	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//	 	return new CorsFilter(urlBasedCorsConfigurationSource);
+//	 }
 
 
 }
